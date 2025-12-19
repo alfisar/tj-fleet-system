bfd0d72ac449bf9672448a5d5c794dbf428ec0e1
alfisar <alfisar589@gmail.com>
2025-12-19 14:57:04 +0700

    [FIXING] : json name for struct VehicleLocation

--- mqtt-publisher/domain/mqtt.go
+++ mqtt-publisher/domain/mqtt.go
@@ -2,7 +2,7 @@ package domain
 
 type VehicleLocation struct {
 	VehicleID string  `json:"vehicle_id"`
-	Lat       float64 `json:"lat"`
-	Lng       float64 `json:"lng"`
+	Lat       float64 `json:"latitude"`
+	Lng       float64 `json:"longitude"`
 	Timestamp int64   `json:"timestamp"`
 }
